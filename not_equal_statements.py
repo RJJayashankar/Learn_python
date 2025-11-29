f45fc2994c901c0ff8b3a5cc3a7f9554a74e8097
Jayashankar RJ <120076432+eviljay4@users.noreply.github.com>
2025-11-29 19:52:14 +0530

    not equal statement

---
+++ not_equal_statements.py
@@ -0,0 +1,8 @@
+x = int(input("enter a number for the variable x"))
+y = int(input("enter the number for the vairable y"))
+
+if x != y:
+    print("x is not equal to y")
+else: 
+    print("x is equal to y")
+
