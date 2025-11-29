7769e7d6b1540a76452575f5fc0ee4112f30e3f6
Jayashankar RJ <120076432+eviljay4@users.noreply.github.com>
2025-11-29 19:20:18 +0530

    Added the else statement program

---
+++ else_statement.py
@@ -0,0 +1,14 @@
+x = int(input("enter the number for the variable x"))
+y = int(input("enter the number for the variable y"))
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is lesser than y")
+else: 
+    print("x is equal to y")
+
+
+# the accurasy of the code increased from the previous versions as it is not asking 
+# all the conditions seperately but moves to the next condition when the first conditon is false
+#  making it faster
