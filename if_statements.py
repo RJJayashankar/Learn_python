7ee5008306f26a75b68c0d4e093976fca61af06c
Jayashankar RJ <120076432+eviljay4@users.noreply.github.com>
2025-11-29 18:55:00 +0530

    Initial commit

---
+++ if_statements.py
@@ -0,0 +1,9 @@
+x = int(input("enter a number for the variable x"))
+y = int(input("enter a number for the variable y"))
+
+if x < y:
+    print("x is lesser than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x and y are the same")
