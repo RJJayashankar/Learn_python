c675d2eb3ff8b5be124f1f7794921a4f761ba99e
Jayashankar RJ <120076432+eviljay4@users.noreply.github.com>
2025-11-29 19:09:57 +0530

    else if statement progarm

---
+++ else_statements.py
@@ -0,0 +1,25 @@
+x = int(input("enter the number for the variable x: "))
+y = int(input("entrr the number for the variable y: "))
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is smaller than y")
+elif x == y:
+    print("x and yare equal")
+
+"""
+PS C:\Users\Jayashankar Jayaram\Downloads\practice_python> python else_statements.py
+enter the number for the variable x: 1
+entrr the number for the variable y: 2
+x is smaller than y
+PS C:\Users\Jayashankar Jayaram\Downloads\practice_python> python else_statements.py
+enter the number for the variable x: 2
+entrr the number for the variable y: 1
+x is greater than y
+PS C:\Users\Jayashankar Jayaram\Downloads\practice_python> python else_statements.py
+enter the number for the variable x: 1
+entrr the number for the variable y: 1
+x and yare equal
+PS C:\Users\Jayashankar Jayaram\Downloads\practice_python>    
+"""
