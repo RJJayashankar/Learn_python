7fab3a2e37e8f1d0ad11a05345aefed01e7686fe
Jayashankar RJ <120076432+eviljay4@users.noreply.github.com>
2025-12-01 22:32:12 +0530

    Added the while loop program

---
+++ while_loop.py
@@ -0,0 +1,16 @@
+def main():
+    no_of_meows = int(input("enter the number of meows: "))
+    count(no_of_meows)
+
+
+def count(n):
+    i = 0
+    while i < n:
+        print("meow")
+        i += 1
+
+main()
+
+
+
+
