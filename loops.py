6a9d74ccb673520f70198edc3d750941b07c7a17
Jayashankar RJ <120076432+eviljay4@users.noreply.github.com>
2025-12-01 21:32:29 +0530

    Added the program loops.py and match_case.py

---
+++ loops.py
@@ -0,0 +1,12 @@
+def main():
+    print_meow()
+
+
+
+
+
+def print_meow():
+    print("meow meow meow")
+
+
+main()
---
+++ match_case.py
@@ -0,0 +1,11 @@
+name = input("enter the players name: ")
+
+match name:
+    case "virat":
+        print("RCB")
+    case "Dhoni":
+        print("CSK")
+    case "rohit":
+        print("MI")
+    case _:
+        print("I don't know")
